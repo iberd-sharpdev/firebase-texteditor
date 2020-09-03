418e546a3c2d78bf0f80c20321a12ae987dcfd8a
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 13:46:14 +0300

    1-auth: integrate Firebase into app (using credentials from environment)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { environment } from '@env/environment';
+import { HeaderComponent, HomeComponent, NotFoundComponent } from '@layout/index';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent, HomeComponent, NotFoundComponent } from '@layout/index';
 
 const LAYOUT_COMPONENTS = [HeaderComponent, HomeComponent, NotFoundComponent];
+
+const FIREBASE_INTEGRATION = [AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule];
 
 @NgModule({
     declarations: [AppComponent, ...LAYOUT_COMPONENTS],

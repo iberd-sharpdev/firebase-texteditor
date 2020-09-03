089e78e657ae3cd09fbf5b81d0af0342474809ae
iberd-sharpdev <35920456+iberd-sharpdev@users.noreply.github.com>
2020-09-03 16:19:36 +0500

    added mathjax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { MathContent } from 'src/common/math.interface';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    mathLatex: MathContent = {
+        latex: 'When $a \\ne 0$, there are two solutions to $\\frac{5}{9}$'
+    };
+
+    textChange(event) {
+        this.mathLatex = {
+            latex: event.target.value
+        }
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MathModule } from './mathjax/math.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, MathModule.forRoot()],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }

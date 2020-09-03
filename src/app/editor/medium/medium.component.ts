a74889e2e64f2b7dc5bba3d77ed3a29e662f462d
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 15:08:29 +0300

    2-editor: make basic version for "medium editor" (text input and visual part)

--- src/app/editor/medium/medium.component.ts
+++ src/app/editor/medium/medium.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { MediumEditor } from 'medium-editor';
 
 @Component({
     selector: 'app-medium',
     templateUrl: './medium.component.html',
     styleUrls: ['./medium.component.scss']
 })
-export class MediumComponent implements OnInit {
+export class MediumComponent implements OnInit, AfterViewInit {
+    @ViewChild('editable', { static: true }) editable: ElementRef;
+
+    public editor: any;
 
     constructor() { }
 
     ngOnInit(): void {
+        console.log('init');
     }
 
+    ngAfterViewInit(): void {
+        this.editor = new MediumEditor(this.editable.nativeElement);
+    }
 }

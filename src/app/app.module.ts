3a445a298ce78d9854ccf0fcfc72d7bc5eef2c50
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 13:39:20 +0300

    1-auth: configure routing, pages "home" and "not-found" (+ update header)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent, HomeComponent, NotFoundComponent } from '@layout/index';
+
+const LAYOUT_COMPONENTS = [HeaderComponent, HomeComponent, NotFoundComponent];
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ...LAYOUT_COMPONENTS],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }

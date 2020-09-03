b7da302f1510effbcfa5d1ea872e7c61f6545f0f
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 22:56:19 +0300

    3-sync: make basic synchronization editor with firebase

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { MathContent } from '@src/common/math.interface';
 
@@ -7,6 +9,8 @@ import { MEDIUM_CONFIG } from './medium-config';
 
 import * as htmlToText from 'html-to-text';
 import { MediumEditor } from 'medium-editor';
+import { AuthService } from '../core/services';
+import { UserInfoType } from '../core/models';
 
 @Component({
     selector: 'app-editor',
@@ -16,6 +20,7 @@ import { MediumEditor } from 'medium-editor';
 export class EditorComponent implements OnInit, OnDestroy {
     @ViewChild('editable', { static: true }) editable: ElementRef;
 
+    public currentUser: UserInfoType;
     public editor: MediumEditor;
     public mathLatex: MathContent = { latex: 'latex placeholder' };
 
@@ -27,8 +32,15 @@ export class EditorComponent implements OnInit, OnDestroy {
 
     private editorSub$: any;
     private typing$ = new Subject();
+    private unsubscribe$ = new Subject();
+
+    constructor(
+        private authService: AuthService,
+        private realtimeFirebaseDB: AngularFireDatabase,
+    ) { }
 
     ngOnInit(): void {
+        this.currentUser = this.authService.currentUser$.getValue();
         this.editor = new MediumEditor(this.editable.nativeElement, MEDIUM_CONFIG);
 
         this.editorSub$ = this.editor
@@ -46,6 +58,12 @@ export class EditorComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.editorSub$.unsubscribe('editableInput', null);
+    }
+
+    public onPush(): void {
+        const editorHTML = String(this.editable.nativeElement.innerHTML);
+        const path = `users/${this.currentUser.uid}/editor`;
+        this.realtimeFirebaseDB.object(path).set(editorHTML);
     }
 
     // TODO: delete this block

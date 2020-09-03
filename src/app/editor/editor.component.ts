509650d06c525d9270bf03df0de6c6bc31ffbbd6
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 21:31:43 +0300

    * return test example for Latex to "editor" page

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { MathContent } from '@src/common/math.interface';
 
@@ -12,27 +13,46 @@ import { MediumEditor } from 'medium-editor';
     templateUrl: './editor.component.html',
     styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
     @ViewChild('editable', { static: true }) editable: ElementRef;
 
     public editor: MediumEditor;
+    public mathLatex: MathContent = { latex: 'latex placeholder' };
 
-    public mathLatex: MathContent = {
+    // TODO: delete this block
+    public mathLatexDemo: MathContent = {
         latex: 'When $a \\ne 0$, there are two solutions to $\\frac{5}{9}$'
     };
+    // ---
+
+    private editorSub$: any;
+    private typing$ = new Subject();
 
     ngOnInit(): void {
         this.editor = new MediumEditor(this.editable.nativeElement, MEDIUM_CONFIG);
 
-        this.editor.subscribe('editableInput', (event: InputEvent) => {
-            const editorHTML = String(this.editable.nativeElement.innerHTML);
+        this.editorSub$ = this.editor
+            .subscribe('editableInput', (event: InputEvent) => {
+                console.log('new symbol =>', event.data);
+                this.typing$.next();
 
-            console.log('new symbol =>', event.data);
-            console.log('editor content =>', editorHTML);
-
-            this.mathLatex = {
-                latex: htmlToText.fromString(editorHTML),
-            };
-        });
+                const editorHTML = String(this.editable.nativeElement.innerHTML);
+                console.log('editor content =>', editorHTML);
+                this.mathLatex = {
+                    latex: htmlToText.fromString(editorHTML),
+                };
+            });
     }
+
+    ngOnDestroy(): void {
+        this.editorSub$.unsubscribe('editableInput', null);
+    }
+
+    // TODO: delete this block
+    public textChange(event): void {
+        this.mathLatexDemo = {
+            latex: event.target.value
+        };
+    }
+    // ---
 }

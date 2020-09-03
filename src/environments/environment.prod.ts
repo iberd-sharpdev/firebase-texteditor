f70d43169aabdd49f1a3256b8f471bdd66357a8c
iberd-sharpdev <iberd@sharp-dev.net>
2020-09-03 13:10:16 +0300

    1-auth: install firebase dependencies and set firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
     production: true,
+
+    firebase: {
+        apiKey: 'AIzaSyCSKzy7Dy33sePdDhmOhrvKJr6MJjSVuUs',
+        authDomain: 'fir-texteditor-2b21a.firebaseapp.com',
+        databaseURL: 'https://fir-texteditor-2b21a.firebaseio.com',
+        projectId: 'fir-texteditor-2b21a',
+        storageBucket: 'fir-texteditor-2b21a.appspot.com',
+        messagingSenderId: '669161697024',
+        appId: '1:669161697024:web:e00b6bc88b9ab2a7384de5',
+    },
 };
